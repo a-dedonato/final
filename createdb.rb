c032d4b8da64f4ca4051184d5765ff599d20de53
a-dedonato <dedonato.a@gmail.com>
2020-06-11 04:26:42 +0000

    Initial Commit

--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,42 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :classes do
   primary_key :id
   String :title
-  String :description, text: true
-  String :date
+  String :course_code
+  String :term
+  String :professor
+  String :meeting_pattern
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :reviews do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :class_id
+  foreign_key :user_id
+  Integer :rating
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+class_table = DB.from(:classes)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+class_table.insert(title: "Finance I", 
+                    course_code: "FINC-430",
+                    term: "Fall 2019",
+                    professor: "Prof. Darius Winkel",
+                    meeting_pattern: "Mon/Thurs 1:30 pm - 3:00 pm",
+                    location:"2211 Campus Dr, Evanston, IL 60208")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+class_table.insert(title: "Leading in Organizations", 
+                    course_code: "MORS-430",
+                    term: "Winter 2019",
+                    professor: "Prof. Elijah Davis",
+                    meeting_pattern: "Tues/Fri 10:30 am - 12:00 pm",
+                    location:"95 Merrick Way Coral Gables, Florida 33134")
